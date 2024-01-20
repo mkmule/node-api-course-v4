f6e4f52f03773f6ca6ce51b29fe080bfb0eccafd
Serhiy <serhiy.mos@gmail.com>
2024-01-20 21:08:28 +0400

    Custom middleware example

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,15 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Custom middleware example
+// app.use((req, res, next) => {
+// req['some_secret'] = 'doggy';
+// next();
+
+// res.status(401);
+// res.json({ message: 'Forbidden' });
+// });
+
 app.get('/', async (req, res) => {
   console.log('hello from express');
   res.status(200);
