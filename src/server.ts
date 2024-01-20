07ca4c56b9dcca310a783b2daa70aa3c8f045e5d
Serhiy <serhiy.mos@gmail.com>
2024-01-20 21:04:10 +0400

    Added json and urlencoded middlewares

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,9 @@ import router from './router';
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', async (req, res) => {
   console.log('hello from express');
   res.status(200);
